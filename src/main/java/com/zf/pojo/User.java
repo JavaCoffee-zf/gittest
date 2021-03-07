ba08c7581a5753d517c7cab0b4992d78208edbd1
zhangfan <hello@zhangfan.cn>
2021-03-07 12:12:02 +0800

    init gittest project

---
+++ src/main/java/com/zf/pojo/User.java
@@ -0,0 +1,9 @@
+package com.zf.pojo;
+
+/**
+ * @author 张帆
+ * @version 1.0
+ * @date 2021/3/7 12:01
+ */
+public class User {
+}
---
+++ src/test/java/com/zf/test/UserTest.java
@@ -0,0 +1,9 @@
+package com.zf.test;
+
+/**
+ * @author 张帆
+ * @version 1.0
+ * @date 2021/3/7 12:02
+ */
+public class UserTest {
+}
